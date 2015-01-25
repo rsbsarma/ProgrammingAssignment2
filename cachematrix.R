d299b28b00fcc1e0ee9ba35b00cd71bb26328215
Sarma <rampalli.aravind@gmail.com>
2015-01-25 19:54:30 +0530

    Submitted the solution

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
@@ -12,4 +20,35 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data.")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
 }
+
+
+##Sample Run
+
+##> x = rbind(c(1, 2), c(-2, 1))
+##> m = makeCacheMatrix(x)
+##> m$get()
+##     [,1] [,2]
+##[1,]    1    2
+##[2,]   -2    1
+##> cacheSolve(m)
+##     [,1] [,2]
+##[1,]  0.2 -0.4
+##[2,]  0.4  0.2
+##> cacheSolve(m)
+##getting cached data.
+##     [,1] [,2]
+##[1,]  0.2 -0.4
+##[2,]  0.4  0.2
+##>
+
+#Reference online inverse calculator to check the answer: http://matrix.reshish.com/inverCalculation.php
